a3a29f2a16204ea3fc9556ac0f1ad313ccd521d7
flotoine <florian.antoine.m@gmail.com>
2024-11-27 16:36:17 +0100

    user resgister setup

--- LeCarnetApp/src/components/Login.vue
+++ LeCarnetApp/src/components/Login.vue
@@ -1,13 +1,27 @@
 <template>
-    <nav v-if="LoginStore.user !== null">
+    <!-- Welcoming message when connected --->
+    <div v-if="LoginStore.user !== null">
         <h2 v-if="username !== null">Bienvenue {{ username }}</h2> 
         <button v-on:click="logout">Logout</button>
-    </nav>
-    <nav v-else>
-        <input type="email" id="email" name="email" placeholder="Your@email.com" v-model="formData.email">
-        <input type="password" id="password" name="password" placeholder="YourPassword" v-model="formData.password">
+    </div>
+    <!--- Register user form displayed when specific button clicked on -->
+    <div class="userform" v-else-if="create_an_account !== null">
+        <h2>Create your account</h2>
+        <input type="email" id="email" name="email" placeholder="Your@email.com" v-model="registerFormData.email">
+        <input type="password" id="password" name="password" placeholder="YourPassword" v-model="registerFormData.password">
+        <input type="firstname" id="firstname" name="firstname" placeholder="First Name" v-model="registerFormData.first_name">
+        <input type="lastname" id="lastname" name="lastname" placeholder="Last Name" v-model="registerFormData.last_name">
+        <button v-on:click="registerUser" >Create an account</button>
+        <button v-on:click="CloseRegisterUserForm" >Back to login</button>
+    </div>
+    <!-- Default login form displayed at start --->
+    <div class="userform" v-else>
+        <h2>Please login to access your trainings</h2>
+        <input type="email" id="email" name="email" placeholder="Your@email.com" v-model="loginFormData.email">
+        <input type="password" id="password" name="password" placeholder="YourPassword" v-model="loginFormData.password">
         <button v-on:click="login" >Connect</button>
-    </nav>
+        <button v-on:click="OpenRegisterUserForm">Create an account</button>
+    </div>
 </template>
 
 <script setup>
@@ -16,25 +30,36 @@ import { useAuth } from '../store/auth.js'
 import { reactive } from 'vue'
 import { ref } from 'vue';
 
+
+/////////////////////////////////////////////////////////
+
+const create_an_account = ref(null); // create an account form visibility status
+
+function OpenRegisterUserForm () {
+    create_an_account.value = true
+}
+
+function CloseRegisterUserForm () {
+    create_an_account.value = null
+}
+
+/////////////////////////////////////////////////////////
+
 const LoginStore = useAuth();
 
-const formData = reactive({
+const loginFormData = reactive({
     email: "",
     password: "",
 });
 
-
-const username = ref("username")
-
-
 async function login()
         {
             let result = await axios.post("http://127.0.0.1:8055/auth/login/",      
-            formData
+            loginFormData
                 ).then(
                     response => {
                         LoginStore.authenticate({
-                        formData
+                            loginFormData
                         })
                         localStorage.setItem("accesstoken",response.data.data.access_token);
                         localStorage.setItem("refreshtoken",response.data.data.refresh_token);
@@ -43,6 +68,10 @@ async function login()
                 ).catch(error => {console.error(error)})       
         }
 
+/////////////////////////////////////////////////////////
+
+const username = ref("username")  /// defines name in welcoming message // Called after successfull login
+
 async function getUser()
         {       
             let result = await axios.get("http://127.0.0.1:8055/users/me",{ headers: {"Authorization" : `Bearer ${localStorage.getItem("accesstoken")}`} })
@@ -50,6 +79,23 @@ async function getUser()
             ).catch(error => {console.error(error)})  
             return {username}
         }
+
+/////////////////////////////////////////////////////////
+
+const registerFormData = reactive({
+    email: "",
+    password: "",
+    first_name: "",
+    last_name: ""
+})
+
+async function registerUser() {
+    let result = await axios.post("http://127.0.0.1:8055/users/register/",registerFormData)
+    .then(res => console.log(res), CloseRegisterUserForm) // returns to classic login view
+    .catch(err => console.error(err))
+}
+
+/////////////////////////////////////////////////////////
 
 async function logout()
         {
@@ -61,12 +107,15 @@ async function logout()
             ).catch(err => console.error(err))
         }
 
+/////////////////////////////////////////////////////////
+
 </script>
 
 
 <style .scoped>
-    nav {
+    .userform {
         display: flex;
+        flex-direction: column;
         gap: 10px;
     }
 </style>
